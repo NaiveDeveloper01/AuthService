07bdaeb8bb857801f683438f86953e0b51ca9281
NaiveDeveloper01 <156093463+NaiveDeveloper01@users.noreply.github.com>
2024-01-09 19:45:03 +0530

    starter auth project

---
+++ App-Auth/src/test/java/com/Socialblog/AppAuth/AppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Socialblog.AppAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
